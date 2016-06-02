de94c1ef57c19e3b00d12a566c934260fdf33e55
StevenTCramer <>
2016-06-02 18:52:38 -0400

    Solution

---
+++ ConsoleApplication1/AmazonBalancer.cs
@@ -0,0 +1,64 @@
+﻿namespace ConsoleApplication1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+
+	class AmazonBalancer
+	{
+		public List<int> Data { get; }
+		public List<int> List1 { get; }
+		public List<int> List2 { get; }
+		public int ArraySize => Data.Count / 2;
+		public int List1Sum	 { get; set; }
+		public int List2Sum { get; set; }
+		public List<int> SortedList => Data.OrderByDescending(i =>  Math.Abs(i)).ToList();
+
+		public AmazonBalancer(List<int> aList)
+		{
+			// Incoming list is supposed to have even number of elements
+			Debug.Assert(aList.Count % 2 == 0);
+
+			Data = aList;
+			List1 = new List<int>();
+			List2 = new List<int>();
+		}
+
+		public void ProcessData()
+		{			
+			foreach (var i in SortedList)
+			{
+				if( (List1Sum > List2Sum && i > 0 ) ||
+						(List1Sum < List2Sum && i < 0 ) && 
+						(List2.Count < ArraySize) ||
+						List1.Count == ArraySize)
+				{
+					List2.Add(i);
+					List2Sum += i;
+				}
+				else
+				{
+					List1.Add(i);
+					List1Sum += i;
+				}
+				Display();
+			}
+		}
+
+		public void Display()
+		{
+			Console.WriteLine($"List1: {String.Join(separator: ",", values: List1)}");
+			Console.WriteLine($"List2: {String.Join(separator: ",", values: List2)}");
+			Console.WriteLine($"List1Sum: {List1Sum}");
+			Console.WriteLine($"List2Sum: {List2Sum}");
+			Console.WriteLine($"List1.Count: {List1.Count}");
+			Console.WriteLine($"List2.Count: {List2.Count}");
+			Console.WriteLine(new String(c: '=', count: 25));
+		}
+	}
+}
+
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var data = new List<int> { 1000, 500, 200, 1, 5, 10, 50, 70, 70, 100 };
+			var amazonBalancer = new AmazonBalancer(data);
+			amazonBalancer.ProcessData();
+
+			data = new List<int> { 1000, 500, 200, 100, 70, 70, 50, 10, 5, 1, 0, -100, -1000, -10000, -500, -300 };
+
+			amazonBalancer = new AmazonBalancer(data);
+			amazonBalancer.ProcessData();
+			//amazonBalancer.Display();
+		}
+	}
+}
